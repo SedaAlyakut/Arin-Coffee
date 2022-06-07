2663348d6c4d6356a178b9f6e84abac9cc9f0679
SedaAlyakut <sedaalyakutt@gmail.com>
2022-06-08 01:01:33 +0300

    Admin model ve register model oluşturuldu. Login tuşu ve yönlendirmesi yapıldı

---
+++ ArinCoffee/WEBUI/AdminLoginModels/AdminLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEBUI.LoginModels
+{
+    public class AdminLoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
