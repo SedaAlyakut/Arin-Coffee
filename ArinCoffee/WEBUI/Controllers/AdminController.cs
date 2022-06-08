42722c3d516cd2a3cd2da6fcf8b3143d4b8c020f
mertburakd <mertburakdervisoglu@gmail.com>
2022-06-08 10:10:34 +0300

    Admin sayfası eklendi o noktadan başlanacak

---
+++ ArinCoffee/WEBUI/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Buisness.Abstract;
+using Entities.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WEBUI.Entities;
+
+namespace WEBUI.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+
+        private UserManager<CustomIdentityUser> _userManager;
+        private readonly IWebHostEnvironment _env2;
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+
+        public AdminController
+            (UserManager<CustomIdentityUser> userManager, IWebHostEnvironment env2, IProductService productService, IOrderService orderService)
+        {           
+            _userManager = userManager;
+            _env2 = env2;
+            _productService = productService;
+            _orderService = orderService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel model = new IndexViewModel();
+            model.products = _productService.GetList();
+            model.orders = _orderService.GetList();
+            return View(model);
+        }
+
+    }
+}
